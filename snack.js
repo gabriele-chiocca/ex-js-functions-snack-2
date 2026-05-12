b8dc6983f9d89f1289f2f9efabe3a69d9f0b2326
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-12 13:02:44 +0200

    snack 2

--- snack.js
+++ snack.js
@@ -1,21 +1,27 @@
 //Snack 1
 
 // 1)
-function sommaNumeri(numero1, numero2) {
-  console.log(numero1 + numero2);
-}
+// function sommaNumeri(numero1, numero2) {
+//   console.log(numero1 + numero2);
+// }
 
-sommaNumeri(1, 2);
+// sommaNumeri(1, 2);
 
 // 2)
 
-const sommadeiNum = function (num1, num2) {
-  console.log(num1 + num2);
-};
+// const sommadeiNum = function (num1, num2) {
+//   console.log(num1 + num2);
+// };
 
-sommadeiNum(1, 2);
+// sommadeiNum(1, 2);
 
 // 3)
-const sommaNum = (numero1, numero2) => console.log(numero1 + numero2);
+// const sommaNum = (numero1, numero2) => console.log(numero1 + numero2);
 
-sommaNum(1, 2);
+// sommaNum(1, 2);
+
+//Snack 2
+
+const quadrato = (num) => console.log(num * 2);
+
+quadrato(2);
