0fc112777a565f2afc84f797d06885601605c91e
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-12 12:57:08 +0200

    First Snack completed

---
+++ snack.js
@@ -0,0 +1,21 @@
+//Snack 1
+
+// 1)
+function sommaNumeri(numero1, numero2) {
+  console.log(numero1 + numero2);
+}
+
+sommaNumeri(1, 2);
+
+// 2)
+
+const sommadeiNum = function (num1, num2) {
+  console.log(num1 + num2);
+};
+
+sommadeiNum(1, 2);
+
+// 3)
+const sommaNum = (numero1, numero2) => console.log(numero1 + numero2);
+
+sommaNum(1, 2);
